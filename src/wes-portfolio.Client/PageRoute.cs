b54f3c8bc53f2dbe92738818589ff7d8221a83cf
WES <21105311+wesimon01@users.noreply.github.com>
2025-05-18 01:48:47 -0400

    initial commit

---
+++ src/wes-portfolio.Client/PageRoute.cs
@@ -0,0 +1,10 @@
+﻿namespace wes_portfolio.Client
+{
+    public class PageRoute
+    {
+        public const string Home = "/";
+        public const string KAFCRoute = "kafc";
+        public const string KYProudRoute = "kyproud";
+        public const string OSVTagRoute = "osvtag";
+    }
+}
---
+++ src/wes-portfolio/Program.cs
@@ -0,0 +1,37 @@
+using MudBlazor.Services;
+using wes_portfolio.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(wes_portfolio.Client._Imports).Assembly);
+
+app.Run();
